48223192dadd02aa03e09fa55dbf5b28de16ef5b
Nathan Cheung <cheungnj@rose-hulman.edu>
2016-01-19 14:18:43 -0500

    reading all image sets now

--- imageFolderReader.m
+++ imageFolderReader.m
@@ -1,11 +1,19 @@
 % Example of reading all the files in a given folder, e.g., TrainSunset. 
 % For the sunset detector, you should keep the images in 4 separate folders: train and test 
 % are separate, and the folder names tell you what the labels are (sunset = +1, non = -1) 
-subdir = './sunsetDetectorImages/TrainSunset';
-fileList = dir(subdir);
+subdir1 = './sunsetDetectorImages/TrainSunset';
+fileList1 = dir(subdir1);
+subdir2 = './sunsetDetectorImages/TrainNonsunsets';
+fileList2 = dir(subdir2);
+subdir3 = './sunsetDetectorImages/TestSunset';
+fileList3 = dir(subdir3);
+subdir4 = './sunsetDetectorImages/TestNonsunsets';
+fileList4 = dir(subdir4);
+
 % files 1 and 2 are . (current dir) and .. (parent dir), respectively, 
 % so we start with 3.
-features = zeros(size(fileList, 1) - 2, 294);
+matrix_size = size(fileList1, 1) + size(fileList2, 1) + size(fileList3, 1) + size(fileList4, 1) - 8;
+features = zeros(matrix_size, 294);
 for i = 3:size(fileList)
     img = imread([subdir  '/'  fileList(i).name]);
     features(i-2, :) = extractFeature(img);
@@ -13,4 +21,3 @@ for i = 3:size(fileList)
     % Hint: debug the loop body on 1-2 images BEFORE looping over lots of them...
     normal_features = normalizeFeatures01(features);
 end
-
