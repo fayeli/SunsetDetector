023fa1bca6cea18064e9005705af8b0199a5e66d
Faye Li Si Fi <lis@rose-hulman.edu>
2016-01-19 16:48:32 -0500

    fix reading and normalizing all features

--- imageFolderReader.m
+++ imageFolderReader.m
@@ -12,17 +12,34 @@ fileList4 = dir(subdir4);
 
 % files 1 and 2 are . (current dir) and .. (parent dir), respectively, 
 % so we start with 3.
-matrix_size = size(fileList1, 1) + size(fileList2, 1) + size(fileList3, 1) + size(fileList4, 1) - 8;
-features = zeros(matrix_size, 294);
-for i = 3:size(fileList)
-    img = imread([subdir  '/'  fileList(i).name]);
+k1 = size(fileList1, 1) - 2;
+k2 = size(fileList1, 1)+size(fileList2, 1) - 4;
+k3 = size(fileList1, 1)+size(fileList2, 1)+size(fileList3, 1) - 6;
+k4 = size(fileList1, 1)+size(fileList2, 1)+size(fileList3, 1) +size(fileList4, 1) - 8;
+
+features = zeros(k4, 294);
+for i = 3:size(fileList1)
+    img = imread([subdir1  '/'  fileList1(i).name]);
     features(i-2, :) = extractFeature(img);
-    % TODO: insert code of function call here to operate on image.
-    % Hint: debug the loop body on 1-2 images BEFORE looping over lots of them...
-    normal_features = normalizeFeatures01(features);
 end
 
-normal_train_sunset = normal_features(1:size(fileList1, 1) - 2, :);
-normal_train_nonsunset = normal_features((size(fileList1, 1) - 1):(size(fileList2, 1) - 2), :);
-normal_test_sunset = normal_features((size(fileList2, 1) - 1):(size(fileList3, 1) - 2), :);
-normal_test_nonsunset = normal_features((size(fileList3, 1) - 1):(size(fileList4, 1) - 2), :);
+for i = 3:size(fileList2)
+    img = imread([subdir2  '/'  fileList2(i).name]);
+    features(i + k1 - 2, :) = extractFeature(img);
+end
+
+for i = 3:size(fileList3)
+    img = imread([subdir3  '/'  fileList3(i).name]);
+    features(i + k2 - 2, :) = extractFeature(img);
+end
+
+for i = 3:size(fileList4)
+    img = imread([subdir4  '/'  fileList4(i).name]);
+    features(i + k3 - 2, :) = extractFeature(img);
+end
+
+normal_features = normalizeFeatures01(features);
+normal_train_sunset = normal_features(1:k1, :);
+normal_train_nonsunset = normal_features(k1+1:k2, :);
+normal_test_sunset = normal_features(k2+1:k3, :);
+normal_test_nonsunset = normal_features(k3+1:k4, :);
